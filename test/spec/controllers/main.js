bd4bf4faad44379a78f9d847b3a8c63f324177dd
Marcelo J. Both <marcelo.both@gmail.com>
2014-07-09 21:32:52 -0300

    NavigationController test of isActive method

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,21 +2,38 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
+  var location, scope;
+
+  // Our module
   beforeEach(module('swFrontApp'));
 
-  var MainCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  // Inject all the dependecies which we need, if necessary mock, stub dependecies
+  beforeEach(inject(function ($controller, $rootScope, $location) {
+    location = $location;
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    $controller('NavigationController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  // Testing is active property
+  describe('isActive', function() {
+    it('returns true when paths are the same', function() {
+      location.path("/test");
+      expect(scope.isActive('/test')).toBeTruthy();
+    });
+    it('returns false when paths are the different', function() {
+      location.path("/different");
+      expect(scope.isActive('/test')).toBeFalsy();
+    });
+    it('returns true when starts with the same word', function() {
+      location.path("/test/1/show");
+      expect(scope.isActive('/test')).toBeTruthy();
+    });
+    it('returns true if it starts with the same word followed by query string', function() {
+      location.path("/test?pg=1");
+      expect(scope.isActive('/test')).toBeTruthy();
+    });
   });
+
 });
